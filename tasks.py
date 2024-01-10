eb21619957061d05f2bdc74a967006fa54f1788e
César Del Solar <delsolar@gmail.com>
2024-01-10 10:20:03 -0500

    remove dependency on glibc if possible

--- tasks.py
+++ tasks.py
@@ -9,5 +9,7 @@ def build(c):
     for os, nickname, arch, executable in [
         ("linux", "linux-x86_64", "amd64", "codecomet"),
     ]:
-        c.run(f"""GOOS={os} GOARCH={arch} go build -ldflags="-w -s" -o {executable}""")
+        c.run(
+            f"""GOOS={os} GOARCH={arch} CGO_ENABLED=0 go build -ldflags="-w -s" -o {executable}"""
+        )
         c.run(f"zip -r codecomet-{tag}-{nickname}.zip ./{executable}")
